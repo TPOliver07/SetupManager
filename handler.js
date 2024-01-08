dd9e82c51cb40f9f3611d58118605838cd2070ee
TPOliver07 <145502864+TPOliver07@users.noreply.github.com>
2024-01-08 17:09:00 -0600

    Added 1.0.0 Library Files

---
+++ handler.js
@@ -0,0 +1,86 @@
+// This script handles page changes and save/load
+
+doc = "main.html"
+
+var elementIDs = [
+"track","date",
+"LFWeight", "RFWeight", "LRWeight", "RRWeight",
+"DriverWeight", "BallastWeight",
+"LFHeight","RFHeight","LRHeight","RRHeight",
+"LFPressure","RFPressure","LRPressure","RRPressure",
+"LFCamber", "RFCamber", "LFCaster", "RFCaster",
+"FStagger","RStagger","LFTireCirc", "RFTireCirc", "LRTireCirc", "RRTireCirc",
+"LFSpring","RFSpring","LRSpring","RRSpring",
+"TireSet","TireAge","ToeIn","FrontBias",
+"LFShock","RFShock","LRShock","RRShock",
+"PanhardAngle","PinionAngle",
+"comments", "QTime", "QPos", "QPole", "RStart", "RFinish", "CarCount"
+]; // List of IDs in the order they should be referenced.
+
+var textFile = null,
+  makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    // returns a URL you can use as a href
+    return textFile;
+  };
+
+function download(url,fileName) {
+  const a = document.createElement('a')
+  a.href = url
+  a.download = fileName // Use a specific filename to make the files verifiable.
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+}
+
+function save() {
+	// Need to get the value of every input.
+	var savedValues = [];
+	// var readable = []; // Readable version of the saveArray.
+	var i = 0
+	while (i < elementIDs.length) {
+		savedValues.push('"'+ document.getElementById(elementIDs[i]).value + '"');
+		// readable.push(elementIDs[i] + ": " + document.getElementById(elementIDs[i]).value);
+		i += 1;
+	}
+	console.log(savedValues.toString());
+	// console.log(readable.toString());
+	
+	download(makeTextFile(savedValues.toString()), savedValues[0] + "_" + savedValues[1] + ".stp");
+}
+var loadedValues = [];
+function load() {
+	var file = document.getElementById("loadPathInput").files[0];
+	
+	if (file) {
+		var reader = new FileReader();
+		reader.readAsText(file, "UTF-8");
+		reader.onload = function (evt) {
+			loadedValues = evt.target.result.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
+			//console.log(evt.target.result.split(","));
+			console.log(loadedValues);
+			
+			// Update data
+			var i = 0
+			while (i < elementIDs.length) {
+				document.getElementById(elementIDs[i]).value = loadedValues[i].substr(1,(loadedValues[i].length)-2);
+				i += 1;
+			}
+		}
+		reader.onerror = function (evt) {
+			alert("error reading file");
+		}
+	}
+	
+	scaleFill();
+	
+}
---
+++ logic.js
@@ -0,0 +1,86 @@
+// This script runs the logic.
+
+doc = "main.html"
+
+var scaleFillRuns = 0;
+var errorFlag = false;
+function scaleFill () {
+	// Read the inputs
+	var LFWeight = document.getElementById("LFWeight").value;
+	var RFWeight = document.getElementById("RFWeight").value;
+	var LRWeight = document.getElementById("LRWeight").value;
+	var RRWeight = document.getElementById("RRWeight").value;
+	// Check for all inputs to be non-zero, non-blank.
+	if (LFWeight.length > 0 && RFWeight.length > 0 && LRWeight.length > 0 && RRWeight.length > 0){
+		// Ensure the background colors are back to normal if necessary.
+		if (errorFlag == true) {
+			document.getElementById("LFParent").style.backgroundColor = "#ffffff";
+			document.getElementById("LRParent").style.backgroundColor = "#ffffff";
+			document.getElementById("RFParent").style.backgroundColor = "#ffffff";
+			document.getElementById("RRParent").style.backgroundColor = "#ffffff";
+			errorFlag = false;
+		}
+		
+		// Write the outputs
+		// Redefine as float values.
+		LFWeight = parseFloat(LFWeight);
+		RFWeight = parseFloat(RFWeight);
+		LRWeight = parseFloat(LRWeight);
+		RRWeight = parseFloat(RRWeight);
+		
+		var TotalWeight = LFWeight + RFWeight + LRWeight + RRWeight;
+		document.getElementById("TotalWeight").innerHTML = TotalWeight.toFixed(0) + " lbs";
+		document.getElementById("CrossWeight").innerHTML = (LRWeight+RFWeight).toFixed(0) + " lbs";
+		document.getElementById("CrossPct").innerHTML = (((LRWeight+RFWeight)/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("RearWeight").innerHTML = (LRWeight+RRWeight).toFixed(0) + " lbs";
+		document.getElementById("RearPct").innerHTML = (((LRWeight+RRWeight)/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("LeftWeight").innerHTML = (LFWeight+LRWeight).toFixed(0) + " lbs";
+		document.getElementById("LeftPct").innerHTML = (((LFWeight+LRWeight)/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("RightWeight").innerHTML = (RFWeight+RRWeight).toFixed(0) + " lbs";
+		document.getElementById("RightPct").innerHTML = (((RFWeight+RRWeight)/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("LFPct").innerHTML = ((LFWeight/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("RFPct").innerHTML = ((RFWeight/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("LRPct").innerHTML = ((LRWeight/TotalWeight)*100).toFixed(2) + "%";
+		document.getElementById("RRPct").innerHTML = ((RRWeight/TotalWeight)*100).toFixed(2) + "%";
+		scaleFillRuns += 1;
+		console.log(scaleFillRuns);
+	}
+	else {
+		// Identify the corner (if scale fill has already ran that is)!
+		if (scaleFillRuns > 0){
+			if (LFWeight.length == 0) {
+				document.getElementById("LFParent").style.backgroundColor = "#ff9292";
+				errorFlag = true;
+			}
+			else if (RFWeight.length == 0) {
+				document.getElementById("RFParent").style.backgroundColor = "#ff9292";
+				errorFlag = true;
+			}
+			else if (LRWeight.length == 0) {
+				document.getElementById("LRParent").style.backgroundColor = "#ff9292";
+				errorFlag = true;
+			}
+			else if (RRWeight.length == 0) {
+				document.getElementById("RRParent").style.backgroundColor = "#ff9292";
+				errorFlag = true;
+			}
+		}
+		
+		// Empty the values to prevent nonsense measurements
+		document.getElementById("TotalWeight").innerHTML = "lbs";
+		document.getElementById("CrossWeight").innerHTML = "lbs";
+		document.getElementById("CrossPct").innerHTML = "%";
+		document.getElementById("RearWeight").innerHTML = "lbs";
+		document.getElementById("RearPct").innerHTML = "%";
+		document.getElementById("LeftWeight").innerHTML = "lbs";
+		document.getElementById("LeftPct").innerHTML = "%";
+		document.getElementById("RightWeight").innerHTML = "lbs";
+		document.getElementById("RightPct").innerHTML = "%";
+		document.getElementById("LFPct").innerHTML = "%";
+		document.getElementById("RFPct").innerHTML = "%";
+		document.getElementById("LRPct").innerHTML = "%";
+		document.getElementById("RRPct").innerHTML = "%";
+		
+		return console.log("Missing a corner weight!");
+	}
+}
